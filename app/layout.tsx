7daecc0049981dc483bff6609a19a00f58a9b654
Uzair Mughal <151128570+uzairdeveloper223@users.noreply.github.com>
2025-07-07 09:30:40 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Drag The Task',
+  description: 'interactive todo app with drag and drop feature',
+  generator: 'awais',
 }
 
 export default function RootLayout({
